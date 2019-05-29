1ee446f5366ba9b39ff71416a5875054954e447f
MineCoinChain <1105541797@qq.com>
2019-05-29 19:40:25 +0800

    添加角色创建

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import "zinx/znet"
+import (
+	"zinx/znet"
+	"zinx/ziface"
+	"fmt"
+	"MMOGameServe/core"
+)
 
+//定义一个hook函数
+func OnConnectionStart(conn ziface.IConnection) {
+	fmt.Println("测试前后台链接是否正常")
+	npc := core.NewPlayer(conn)
+	npc.ReturnPid()
+	npc.ReturnPlayerPosition()
+
+
+}
 func main() {
 	s := znet.NewServer("MMOGame")
 
 	//hook函数
-
+	s.AddOnConnStart(OnConnectionStart)
 	//注册路由
 
 	s.Serve()
